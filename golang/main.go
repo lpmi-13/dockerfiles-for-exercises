c6df486e856976fb69ef8179c38ad77a0fc99937
Adam Leskis <leskis@gmail.com>
2024-11-09 22:19:30 +0000

    switch to port 8000

--- golang/main.go
+++ golang/main.go
@@ -5,7 +5,6 @@ import (
 )
 
 func main() {
-	// Create a new Gin router
 	r := gin.Default()
 
 	// Define a route for the root path
@@ -13,6 +12,5 @@ func main() {
 		c.String(200, "OK")
 	})
 
-	// Start the server on port 8080
-	r.Run(":8080")
+	r.Run(":8000")
 }
